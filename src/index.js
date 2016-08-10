7919e84659bde8c7f47f8257781c3550e2025d4c
Blake Netzeband <blake.netzeband@gmail.com>
2016-08-10 11:18:31 -0600

    rendered 'hello world' and created component folder

--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-<<<<<<< HEAD
-// Create new components that create HTML
-const App = function () {
-  return <div>Hi!</div>;
-}
-
-// places HTML in the DOM
-=======
 import React from 'react';
 import ReactDOM from 'react-dom';
-import YTSearch from 'youtube-api-search';
 
-import SearchBar from './components/search_bar';
+// Create new components that create HTML
+const App = () => <div>Hello Ida</div>;
 
-const YOUTUBE_API_KEY = '';
-
-YTSearch({key: YOUTUBE_API_KEY, term: 'surfboards'}, function(data){
-  console.log(data);
-})
-
-// Create a new component. This component should produce some html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
-}
-
-// Take this component's generated HTML and put it on the page (in DOM)
+// places HTML in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
->>>>>>> 886bdf064a52aecd4e8f0c4680c530f897de820c
