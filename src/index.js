7b712a1b6699233664487b5b1ca43d70e9259789
Blake Netzeband <blake.netzeband@gmail.com>
2016-08-10 12:11:54 -0600

    created youtube search

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyBpQc5Ijd1bXG0nmIPVn-0qcKYwkl7Hqtk';
+
+YTSearch({key: API_KEY}, function (data) {
+  console.log(data);
+});
 
 // Create new components that create HTML
 const App = () => {
