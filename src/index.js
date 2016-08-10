e07dda6d794f5830a885ee780bff1a41705da7d5
Blake Netzeband <blake.netzeband@gmail.com>
2016-08-10 11:54:38 -0600

    added state

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBpQc5Ijd1bXG0nmIPVn-0qcKYwkl7Hqtk';
 
 // Create new components that create HTML
-const App = () => <div>Hello Ida</div>;
+const App = () => {
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+}
 
 // places HTML in the DOM
 ReactDOM.render(<App />, document.querySelector('.container'));
