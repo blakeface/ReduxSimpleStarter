046951fb0c7869c7d4a2337e25b7880895f4b411
Blake Netzeband <blake.netzeband@gmail.com>
2016-08-15 15:37:03 -0600

    added book reducer and container

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Harry Potter' },
+    { title: 'Javascript: The Good Parts' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' },
+  ]
+}
